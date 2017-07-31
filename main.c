77c832f11b1e0eafcb5eaa3b8a9fdd6e5e7f9865
Kobus COETZEE <kcoetzee@e4r23p3.wtc.co.za>
2017-07-31 14:15:02 +0200

    pre breadth-first

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: kcoetzee <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/31 07:23:45 by kcoetzee          #+#    #+#             */
-/*   Updated: 2017/07/31 11:03:30 by kcoetzee         ###   ########.fr       */
+/*   Updated: 2017/07/31 14:14:33 by kcoetzee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,32 +19,6 @@ void	read_data()
 
 int		find_shortest_path(t_room *start, t_room *end, int min_count, int count, t_room *caller)
 {
-	printf("I ran!\n");
-	int i = 0;
-	int lowest_index = 0;
-
-	t_room *result;
-	count++;
-
-	if (start == end)
-	{
-		return (lowest_index);
-	}
-
-	while (i < start->n_links)
-	{
-		if (start->links[i] != caller)
-		{
-			count = find_shortest_path(start->links[i], end, min_count, count, start);
-			if (count < min_count)
-			{
-				lowest_index = i;
-				min_count = count;
-			}
-		}
-		i++;
-	}
-	return (count);
 
 }
 
@@ -52,29 +26,12 @@ int		main(int argc, char **argv)
 {
 	// Create the rooms
 	char id[] = "2";
-	t_room	*room_0		= room_create("r0", 2, 2);
-	t_room	*room_1 	= room_create("r1", 3, 1);
-	t_room	*room_2		= room_create("r2", 1, 2);
-	t_room	*room_3		= room_create("r3", 1, 1);
-
-	// Link the rooms
-	room_add_link(room_0, room_1);
-	room_add_link(room_0, room_2);
-	room_add_link(room_0, room_3);
+	t_room	*room_A		= room_create("A", 2, 2);	
+	t_room	*room_B		= room_create("B", 2, 2);
+	t_room	*room_C		= room_create("C", 2, 2);
+	t_room	*room_C		= room_create("C", 2, 2);
+	t_room	*room_C		= room_create("C", 2, 2);
 	
-	room_add_link(room_1, room_0);
-	room_add_link(room_1, room_3);
-	
-	room_add_link(room_2, room_0);
-
-	room_add_link(room_3, room_0);
-	room_add_link(room_3, room_1);
-
-	print_room(room_0);
-	print_room(room_1);
-	print_room(room_2);
-	print_room(room_3);
-
 	int test = find_shortest_path(room_0, room_3, 10, 0, NULL);
 	printf("Result: %d\n", test);
 	return (0);
