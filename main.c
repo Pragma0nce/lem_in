13b502a4fc3c51f46962cb34f65cce7f8b8eeb6c
Kobus COETZEE <kcoetzee@e4r23p3.wtc.co.za>
2017-07-31 11:04:15 +0200

    working shortest path algorithm

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: kcoetzee <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/31 07:23:45 by kcoetzee          #+#    #+#             */
-/*   Updated: 2017/07/31 09:23:08 by kcoetzee         ###   ########.fr       */
+/*   Updated: 2017/07/31 11:03:30 by kcoetzee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,37 @@
 
 void	read_data()
 {
+
+}
+
+int		find_shortest_path(t_room *start, t_room *end, int min_count, int count, t_room *caller)
+{
+	printf("I ran!\n");
+	int i = 0;
+	int lowest_index = 0;
+
+	t_room *result;
+	count++;
+
+	if (start == end)
+	{
+		return (lowest_index);
+	}
+
+	while (i < start->n_links)
+	{
+		if (start->links[i] != caller)
+		{
+			count = find_shortest_path(start->links[i], end, min_count, count, start);
+			if (count < min_count)
+			{
+				lowest_index = i;
+				min_count = count;
+			}
+		}
+		i++;
+	}
+	return (count);
 
 }
 
@@ -43,5 +74,8 @@ int		main(int argc, char **argv)
 	print_room(room_1);
 	print_room(room_2);
 	print_room(room_3);
+
+	int test = find_shortest_path(room_0, room_3, 10, 0, NULL);
+	printf("Result: %d\n", test);
 	return (0);
 }
